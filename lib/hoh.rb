b1549bf014eeefd30f26a7f5be4c1d4f3eaf88e6
Brooke Bachman <bbachman1994@gmail.com>
2019-11-11 21:27:46 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,35 +1,4 @@
-def Hoh
-  {
-  :PORTION_1 => {
-  :label => "Kingdom",
-  :sub_category => {
-  :label => "Phylum",
-  :sub_category => {
-  :label => "Class"
-  }
-   }
-    }
-  }
 
- :PORTION_2 => {
- :label => "Order"
-}
-
-
-:PORTION_3 => {
-  :label => "Family",
-  :sub_category => {
-    :label => "Genus"
- }
- }
-
-
-:PORTION_4 => {
-:label => "Species",
-:sub_category => nil
-}
-
-end
 
 
 # Build a nested HoH of the classification of biological life on Earth based
@@ -63,8 +32,29 @@ end
 #   label: "Species",
 #   sub_category: nil
 # }
-
 def naming_system
+{
+  :sub_category => {
+    :label => "Kingdom",
+    :sub_category => {
+      :label => "Phylum",
+      :sub_category => {
+        :label => "Order",
+        :sub_category => {
+          :label => "Family",
+          :sub_category => {
+            :label => "Genus",
+            :sub_category => {
+              :label => "Species",
+              :sub_category => nil
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
   # Remember:
   #  Kingdom
   #  Phylum
