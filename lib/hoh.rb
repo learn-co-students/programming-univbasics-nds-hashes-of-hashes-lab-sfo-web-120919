a838ef3ba3efdfb0c9d7ca56503f1392a1de7059
Brooke Bachman <bbachman1994@gmail.com>
2019-11-11 20:51:28 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,3 +1,37 @@
+def Hoh
+  {
+  :PORTION_1 => {
+  :label => "Kingdom",
+  :sub_category => {
+  :label => "Phylum",
+  :sub_category => {
+  :label => "Class"
+  }
+   }
+    }
+  }
+
+ :PORTION_2 => {
+ :label => "Order"
+}
+
+
+:PORTION_3 => {
+  :label => "Family",
+  :sub_category => {
+    :label => "Genus"
+ }
+ }
+
+
+:PORTION_4 => {
+:label => "Species",
+:sub_category => nil
+}
+
+end
+
+
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
@@ -13,18 +47,18 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
